3f943cc55ee1b0c86db25fb4919bff36c4f46af1
Alice Wenban <Alice.Wenban@softwire.com>
2020-11-17 15:02:44 +0000

    fizzbuzz with options

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,37 +1,42 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 255; i++) {
+
+        List<String> arrayargs=Arrays.asList(args);
+
+        System.out.println("Max number?");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
             String result = "";
-            if (i % 3 == 0) {
+            if (i % 3 == 0 && arrayargs.contains("3")) {
                 result = result + "Fizz";
             }
-            if (i % 13 == 0) {
+            if (i % 13 == 0 && arrayargs.contains("13")) {
                 result = result + "Fezz";
             }
-            if (i % 5 == 0) {
+            if (i % 5 == 0 && arrayargs.contains("5")) {
                 result = result + "Buzz";
             }
-            if (i % 7 == 0) {
+            if (i % 7 == 0 && arrayargs.contains("7")) {
                 result = result + "Bang";
             }
-            if (i % 11 == 0) {
+            if (i % 11 == 0 && arrayargs.contains("11")) {
                 if (result.contains("Fezz")){
                     result="FezzBong";
                 }else {
                     result = "Bong";
                 }
             }
-            if(i % 17 ==0){
-                int rlen= result.length()/4;
-                String tempresult="";
-                for(int j=rlen;j>0;j--){
-                    int key = 4*j;
-                    tempresult=tempresult+result.substring(key-4,key);
-                }
-                result=tempresult;
+            if(i % 17 ==0 && arrayargs.contains("17")){
+                result=buzzreverse(result);
             }
 
             if (result.equals("")) {
@@ -39,5 +44,14 @@ public class Main {
             }
             System.out.println(result);
         }
+    }
+    public static String buzzreverse(String string){
+        int rlen= string.length()/4;
+        String tempresult="";
+        for(int j=rlen;j>0;j--){
+            int key = 4*j;
+            tempresult=tempresult+string.substring(key-4,key);
+        }
+        return tempresult;
     }
 }
